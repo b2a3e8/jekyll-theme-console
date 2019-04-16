167426f838460f5d79ac3927cde9d5e7f626cdbc
b2a3e8 <31370519+b2a3e8@users.noreply.github.com>
2019-04-16 19:22:19 +0200

    Optimise style switching logic

--- assets/style-switcher.js
+++ assets/style-switcher.js
@@ -1,13 +1,25 @@
 function switchDarkLightStyles () {
-    var cssIdLight = 'main-light';
-    var cssIdDark = 'main-dark';
-    if (document.getElementById(cssIdLight).hasAttribute('disabled')) {
+    if (document.getElementById('main-light').hasAttribute('disabled')) {
         // enable light style
-        document.getElementById(cssIdLight).removeAttribute('disabled');
-        document.getElementById(cssIdDark).setAttribute('disabled', true);
+        document.getElementById('main-light').removeAttribute('disabled');
+        document.getElementById('main-dark').setAttribute('disabled', 'disabled');
+        sessionStorage.setItem('style', 'main-light');
     } else {
         // enable dark style
-        document.getElementById(cssIdDark).removeAttribute('disabled');
-        document.getElementById(cssIdLight).setAttribute('disabled', true);
+        document.getElementById('main-dark').removeAttribute('disabled');
+        document.getElementById('main-light').setAttribute('disabled', 'disabled');
+        sessionStorage.setItem('style', 'main-dark');
     }
 }
+function setStyleByUsersPreference () {
+    let userPref = sessionStorage.getItem('style');
+    if (userPref === 'main-light') {
+        document.getElementById('main-light').removeAttribute('disabled');
+        document.getElementById('main-dark').setAttribute('disabled', 'disabled');
+    }
+    if (userPref === 'main-dark') {
+        document.getElementById('main-dark').removeAttribute('disabled');
+        document.getElementById('main-light').setAttribute('disabled', 'disabled');
+    }
+}
+setStyleByUsersPreference();
